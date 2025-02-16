917e312a6ae81e5e27afefa509948c2468f77006
suhanlakshakar89 <suhanlakshakar@gmail.com>
2025-02-16 22:48:25 +0530

    Done

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "Random_Password_Genrator",
+  base: "/Random_Password_Genrator/",
   plugins: [react()],
 })
